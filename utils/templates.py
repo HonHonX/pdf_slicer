92ce6ef1c5576d1df98644d8dfea8073d50696ae
Sherry <119943082+HonHonX@users.noreply.github.com>
2024-02-10 20:28:54 +0100

    Update templates.py

--- utils/templates.py
+++ utils/templates.py
@@ -107,12 +107,10 @@ def dhl_parcel(filename, images, index, instant_download=False):
 
     with st.sidebar:
         # Download button for the cropped image
-        if st.button("Download cropped images (selected file)", type='primary') or instant_download:
+        # if st.button("Download cropped images (selected file)", type='primary') or instant_download:
             # Save the image
-            cropped_image_1.save("/Downloaded_Images/" +filename + "_1.png",format="PNG")
-            cropped_image_2.save("/Downloaded_Images/" + filename + "_2.png",format="PNG")
-            if international_parcel:
-                cropped_image_3.save("/Downloaded_Images/" + filename + "_3.png",format="PNG")
-    
-
-
+            # cropped_image_1.save("/Downloaded_Images/" +filename + "_1.png",format="PNG")
+            # cropped_image_2.save("/Downloaded_Images/" + filename + "_2.png",format="PNG")
+            # if international_parcel:
+            #     cropped_image_3.save("/Downloaded_Images/" + filename + "_3.png",format="PNG")
+            st.download_button(label="Download cropped images (selected file)", data=cropped_image_1, file_name=filename + "_1.png")
