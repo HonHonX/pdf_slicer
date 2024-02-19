0dfd255cff1940197c2c6a75e0ac96666d0f9f6c
HonHonX <buch.sharon@googlemail.com>
2024-02-19 21:38:24 +0100

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,8 +51,11 @@ with st.sidebar:
 
             value = streamlit_image_coordinates(
             images[page_number-1],
+            width = 500,
             key="local",
             )
+
+            ratio = images[page_number-1].width/500
 
             st.write(value['x'])
             st.write(images[page_number-1].width)
@@ -69,7 +72,7 @@ with st.sidebar:
                     load_template.dhl_parcel(filename, images, 1)
                 
                 if template == 'Custom':
-                    load_template.custom(filename, images, 1, value['x'], value['y'])
+                    load_template.custom(filename, images, 1, value['x']*ratio, value['y']*ratio)
 
             with st.sidebar:
                 st.download_button(label="💾 Download cropped image(s)", data=open(filename+".zip", "rb").read(), file_name=filename+".zip", mime="application/zip", type="primary", use_container_width=True)
